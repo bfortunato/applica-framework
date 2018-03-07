8fe3a615526af543db20e059bd99ddfb60f0edbb
bimbobruno <bimbobruno@gmail.com>
2018-03-07 09:46:09 +0100

    Bug fixing

--- framework-indexing/src/main/java/applica/framework/indexing/services/IndexService.java
+++ framework-indexing/src/main/java/applica/framework/indexing/services/IndexService.java
@@ -7,9 +7,12 @@ import applica.framework.indexing.core.IndexedResult;
 public interface IndexService {
 
     <T extends Entity> void index(T entity);
+    void reindexAll(Class<? extends Entity> entityType, Query dataQuery);
 
     void remove(String uniqueId);
 
     <T extends Entity> IndexedResult search(Class<T> entityType, Query query);
+
+
 
 }
