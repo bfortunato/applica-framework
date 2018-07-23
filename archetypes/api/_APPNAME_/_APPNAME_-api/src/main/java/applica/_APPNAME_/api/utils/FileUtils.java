064236da7f9585a5d922d794bfa91632df975e5b
a.lovicario90@gmail.com <a.lovicario90@gmail.com>
2018-07-23 14:13:31 +0200

    better internal resource getter method

--- archetypes/api/_APPNAME_/_APPNAME_-api/src/main/java/applica/_APPNAME_/api/utils/FileUtils.java
+++ archetypes/api/_APPNAME_/_APPNAME_-api/src/main/java/applica/_APPNAME_/api/utils/FileUtils.java
@@ -1,9 +1,6 @@
 package applica._APPNAME_.api.utils;
 
-import applica.framework.ApplicationContextProvider;
-import applica.framework.library.options.OptionsManager;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,15 +9,8 @@ import java.io.InputStream;
 
 public class FileUtils {
     public static InputStream getResourceFileInputStream(String resourcePath) throws IOException {
-        OptionsManager optionsManager = ApplicationContextProvider.provide().getBean(OptionsManager.class);
-        ResourceLoader resourceLoader = ApplicationContextProvider.provide().getBean(ResourceLoader.class);
-        Resource res = resourceLoader.getResource("classpath:/" + resourcePath);
-        //AesCipher.genKey(res.getFile().getAbsolutePath());
-        if (org.springframework.util.StringUtils.hasLength(optionsManager.get("file.getting.mode")) && optionsManager.get("file.getting.mode").equals("jar"))
-            return res.getInputStream();
-        else {
-            return new FileInputStream(res.getFile().getAbsolutePath());
-        }
+        ClassPathResource classPathResource = new ClassPathResource(resourcePath);
+        return classPathResource.getInputStream();
     }
 
 }
