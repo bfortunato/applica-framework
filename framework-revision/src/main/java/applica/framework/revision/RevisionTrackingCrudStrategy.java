aabbe931a73ce96af62fed7439f329d65b6e4162
a.lovicario90@gmail.com <a.lovicario90@gmail.com>
2018-09-18 10:31:36 +0200

    fix in revision strategy

--- framework-revision/src/main/java/applica/framework/revision/RevisionTrackingCrudStrategy.java
+++ framework-revision/src/main/java/applica/framework/revision/RevisionTrackingCrudStrategy.java
@@ -3,7 +3,6 @@ package applica.framework.revision;
 import applica.framework.ChainedCrudStrategy;
 import applica.framework.Entity;
 import applica.framework.Repository;
-import applica.framework.library.options.OptionsManager;
 import applica.framework.revision.services.RevisionService;
 import applica.framework.widgets.entities.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ public class RevisionTrackingCrudStrategy extends ChainedCrudStrategy {
 
     @Autowired
     private RevisionService entityRevisionService;
-
 
 
     @Override
@@ -26,40 +24,28 @@ public class RevisionTrackingCrudStrategy extends ChainedCrudStrategy {
 
 
         boolean creation = entity.getId() == null;
-        Entity previousEntity = !creation? repository.get(entity.getId()).get() : null;
+        Entity previousEntity = !creation ? repository.get(entity.getId()).get() : null;
 
         super.save(entity, repository);
 
-        Runnable runnable = () -> {
-            try {
-                if (entityRevisionService.isRevisionEnabled(EntityUtils.getEntityIdAnnotation(entity.getClass()))) {
-                    entityRevisionService.createAndSaveRevision(entity, previousEntity);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if (entityRevisionService.isRevisionEnabled(EntityUtils.getEntityIdAnnotation(entity.getClass()))) {
+            Runnable runnable = () -> entityRevisionService.createAndSaveRevision(entity, previousEntity);
+            executeRevisionAction(runnable);
+        }
 
-        };
-        executeRevisionAction(runnable);
     }
 
     @Override
     public <T extends Entity> void delete(Object id, Repository<T> repository) {
         Entity previousEntity = repository.get(id).get();
         super.delete(id, repository);
+        String entityName = EntityUtils.getEntityIdAnnotation(previousEntity.getClass());
+        if (entityRevisionService.isRevisionEnabled(entityName)) {
+            Runnable runnable = () -> entityRevisionService.createAndSaveRevision(null, previousEntity);
+            executeRevisionAction(runnable);
+        }
 
-        Runnable runnable = () -> {
-            try {
-                String entityName = EntityUtils.getEntityIdAnnotation(previousEntity.getClass());
-                if (entityRevisionService.isRevisionEnabled(entityName)) {
-                    entityRevisionService.createRevision(null, previousEntity);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
 
-        executeRevisionAction(runnable);
 
 
 
