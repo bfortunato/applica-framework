e8a32b06731ef1473936612291b69799650389e1
applicadev <atlantis90>
2019-11-25 20:52:36 +0100

    fix save entity permissions

--- scripts/src/main/java/applica/framework/Applica.java
+++ scripts/src/main/java/applica/framework/Applica.java
@@ -1,65 +1,55 @@
 package applica.framework;
 
 import applica.framework.modules.CommandLineParser;
-import applica.framework.modules.HibernateModule;
 import applica.framework.modules.Modules;
-import applica.framework.modules.ProjectModule;
 import org.springframework.util.StringUtils;
-
-import java.util.Properties;
 
 public class Applica {
 
-    public static final String VERSION = "2.1.4-RELEASE";
+    public static final String VERSION = "2.1.3-RELEASE";
 
     public static String frameworkHome = null;
     public static String javaHome = null;
     public static String mavenHome = null;
 
     public static void main(String[] args) {
-        frameworkHome = "/Users/antoniolovicario/Progetti/applica_framework_2_1_4/";
-        ProjectModule module = new ProjectModule();
-        Properties p = new Properties();
-        p.setProperty("name", "jira_report");
-        p.setProperty("archetype", "api");
-        module.create(p);
+        frameworkHome = System.getenv("APPLICAFRAMEWORK_HOME");
+        if (!StringUtils.hasLength(frameworkHome)) {
+            System.err.println("Please set APPLICAFRAMEWORK_HOME environment variable");
+            System.exit(1);
+            return;
+        }
 
-//        if (!StringUtils.hasLength(frameworkHome)) {
-//            System.err.println("Please set APPLICAFRAMEWORK_HOME environment variable");
-//            System.exit(1);
-//            return;
-//        }
-//
-//        javaHome = System.getenv("JAVA_HOME");
-//        if (!StringUtils.hasLength(frameworkHome)) {
-//            System.err.println("Please set JAVA_HOME environment variable");
-//            System.exit(1);
-//            return;
-//        }
-//
-//        mavenHome = System.getenv("M2_HOME");
-//        if (!StringUtils.hasLength(mavenHome)) {
-//            System.err.println("Please set M2_HOME environment variable");
-//            System.exit(1);
-//            return;
-//        }
-//
-//        Modules.instance().scan();
-//
-//        CommandLineParser parser = new CommandLineParser();
-//        try {
-//            parser.parse(args);
-//
-//            if (parser.getCommand().equals("version")) {
-//                System.out.println("Applica Framework version " + VERSION);
-//            } else if (parser.getCommand().equals("help")) {
-//                printUsage();
-//            } else {
-//                Modules.instance().call(parser.getCommand(), parser.getProperties());
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        javaHome = System.getenv("JAVA_HOME");
+        if (!StringUtils.hasLength(frameworkHome)) {
+            System.err.println("Please set JAVA_HOME environment variable");
+            System.exit(1);
+            return;
+        }
+
+        mavenHome = System.getenv("M2_HOME");
+        if (!StringUtils.hasLength(mavenHome)) {
+            System.err.println("Please set M2_HOME environment variable");
+            System.exit(1);
+            return;
+        }
+
+        Modules.instance().scan();
+
+        CommandLineParser parser = new CommandLineParser();
+        try {
+            parser.parse(args);
+
+            if (parser.getCommand().equals("version")) {
+                System.out.println("Applica Framework version " + VERSION);
+            } else if (parser.getCommand().equals("help")) {
+                printUsage();
+            } else {
+                Modules.instance().call(parser.getCommand(), parser.getProperties());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void printUsage() {
