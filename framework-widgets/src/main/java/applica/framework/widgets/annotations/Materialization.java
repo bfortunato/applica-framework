6c749a8dfbc92b77b5d244cbc91a71c0901123be
a.lovicario90@gmail.com <atlantis90>
2021-04-19 14:52:20 +0200

    fixes

--- framework-widgets/src/main/java/applica/framework/widgets/annotations/Materialization.java
+++ framework-widgets/src/main/java/applica/framework/widgets/annotations/Materialization.java
@@ -16,4 +16,6 @@ public @interface Materialization {
 
     //Se true in fase di saveOperation imposta il valore della property da materializzare in base a quella materializzata
     boolean reverseMaterialization() default false;
+
+    boolean indexEnabled() default false;
  }
