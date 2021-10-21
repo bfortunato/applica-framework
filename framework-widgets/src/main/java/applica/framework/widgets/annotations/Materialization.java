ae3ebae5cd089949402007626e6ea6fe92ad8d62
a.lovicario90@gmail.com <atlantis90>
2021-10-21 10:04:02 +0200

    Allineata classe @Materialization al framework 2.3.1

--- framework-widgets/src/main/java/applica/framework/widgets/annotations/Materialization.java
+++ framework-widgets/src/main/java/applica/framework/widgets/annotations/Materialization.java
@@ -8,14 +8,22 @@ import java.lang.annotation.Target;
 /**
  * Usato per materializzare determinate proprietà dell'oggetto in campi transient
  */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Materialization {
     String entityField();
-    Class entityClass();
+
+    @Deprecated
+    Class entityClass() default Object.class;
+
+    //TODO: generare l'entityClass tramite un metodo (vedi l'utilizzo della validationFunction nella classe @Validation)
+    String generateEntityClass() default "";
 
     //Se true in fase di saveOperation imposta il valore della property da materializzare in base a quella materializzata
-    boolean reverseMaterialization() default true;
+    boolean reverseMaterialization() default false;
 
     boolean indexEnabled() default false;
- }
+
+
+}
