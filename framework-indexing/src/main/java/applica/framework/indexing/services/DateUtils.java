fc43ed7c2617f24ac25387dbd8a2761a06ceea91
federicomalvasi <federico.malvasi@applica.guru>
2022-06-27 18:39:58 +0200

    fix timezone conversione date lucene

--- framework-indexing/src/main/java/applica/framework/indexing/services/DateUtils.java
+++ framework-indexing/src/main/java/applica/framework/indexing/services/DateUtils.java
@@ -4,19 +4,24 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class DateUtils {
 
-    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
+    private static DateFormat DATE_FORMAT() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.ITALY);
+        sdf.setTimeZone(TimeZone.getTimeZone("Europe/Rome"));
+        return sdf;
+    }
 
     public static String dateToString(Date value) {
-        return DATE_FORMAT.format(value);
+        return DATE_FORMAT().format(value);
     }
 
     public static Object stringToDate(String stringValue) {
         try {
-            return DATE_FORMAT.parse(stringValue);
+            return DATE_FORMAT().parse(stringValue);
         } catch (ParseException e) {
             //throw new RuntimeException(e);
             return null;
